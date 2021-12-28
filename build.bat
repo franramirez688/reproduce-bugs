0090842070a63809dcdec7394d4f641ca27184ad
Francisco Ramirez de Anton <franchuti688@gmail.com>
2021-12-28 18:43:22 +0100

    Fixed for windows

---
+++ build.bat
@@ -0,0 +1,2 @@
+conan install . -if build --build outdated -pr .profile/clang_x86_64_asan -pr:b .profile/clang_x86_64_asan -e CONAN_CMAKE_GENERATOR=Ninja -c tools.cmake.cmaketoolchain:generator=Ninja
+conan build . -bf build
